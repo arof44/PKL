b58679813c3462768ee918a59408000dce4baa88
arof44 <arafahnur57@gmail.com>
2021-12-31 13:14:57 +0700

    first

---
+++ resources/views/includes/main/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    &copy; ROOMING 2021 - Present by <a href="#" target="#">Polinema</a> <div class="bullet"></div> Laravel Bootstrap &copy; <?php echo date('Y')?> by <a href="#" target="">Muhammad Nur Arafah</a>
+  </div>
+  <div class="footer-right">
+    1.0
+  </div>
+</footer>
